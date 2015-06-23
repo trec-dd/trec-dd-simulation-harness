58c20cae574f65f161e883c3054a63dbbcf712ad
Ian Soboroff <isoboroff@gmail.com>
2015-06-23 16:54:19 -0400

    Removed cube_test from scorer; new code is coming.

--- trec_dd/scorer/__init__.py
+++ trec_dd/scorer/__init__.py
@@ -5,7 +5,6 @@ from trec_dd.scorer.reciprocal_rank_at_recall import reciprocal_rank_at_recall
 from trec_dd.scorer.precision_at_recall import precision_at_recall
 from trec_dd.scorer.modified_precision_at_recall import modified_precision_at_recall
 from trec_dd.scorer.average_err import average_err
-from trec_dd.scorer.cube_test import cube_test
 
 
 def average_err_harmonic(run, label_store):
@@ -28,7 +27,6 @@ available_scorers = {
     'reciprocal_rank_at_recall': reciprocal_rank_at_recall,
     'precision_at_recall': precision_at_recall,
     'modified_precision_at_recall': modified_precision_at_recall,
-    'cube_test': cube_test,
     'average_err_arithmetic': average_err_arithmetic,
     'average_err_harmonic': average_err_harmonic,
     'average_err_arithmetic_binary': average_err_arithmetic_binary,
